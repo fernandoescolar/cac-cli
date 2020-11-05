465978fa31ad09ce6582903e6b096ee5104db49b
Fernando Escolar <fernando.escolar@scrm.eu>
2020-11-05 19:13:37 +0100

    adding random provider

---
+++ src/Cac.Default/Providers/RandomProvider.cs
@@ -0,0 +1,36 @@
+﻿using Cac.Extensibility;
+using Cac.Yaml;
+using System;
+using System.Threading.Tasks;
+
+namespace Cac.Default.Providers
+{
+    [Name("random")]
+    public class RandomProvider : CacProvider<RandomResult>
+    {
+        protected override Task<RandomResult> OnGetValueAsync(IExecutionContext context)
+        {
+            var rnd = new Random();
+            var result = new RandomResult
+            {
+                Double = rnd.NextDouble(),
+                Integer = rnd.Next(),
+                Boolean = rnd.Next(1, 100) > 50
+            };
+
+            return Task.FromResult(result);
+        }
+    }
+
+    public class RandomResult
+    {
+        [YamlProperty("double")]
+        public double Double { get; set; }
+
+        [YamlProperty("integer")]
+        public int Integer { get; set; }
+
+        [YamlProperty("boolean")]
+        public bool Boolean { get; set; }
+    }
+}
