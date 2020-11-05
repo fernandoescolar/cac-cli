c8e2f5ff48a237a8968cd246be4c45e67016fde7
Fernando Escolar <fernando.escolar@scrm.eu>
2020-11-05 17:46:20 +0100

    added "sync" property in providers

--- src/Cac.Interpretation/NodeProcessors/ProvidersNodeProcessor.cs
+++ src/Cac.Interpretation/NodeProcessors/ProvidersNodeProcessor.cs
@@ -21,27 +21,32 @@ namespace Cac.Interpretation.NodeProcessors
             var tasks = new List<Task>();
             foreach (var entry in sequence)
             {
-                tasks.Add(ProcessEntryAsync(entry, context));
+                if (entry is not YamlMappingObject m)
+                    throw new UnexpectedTypeException("Yaml node should be a mapping", entry.Line, entry.Column);
+
+                context.ExpressionEvaluator.EvaluateExpressions(entry);
+                var task = ProcessEntryAsync(m, context);
+                if (IsSyncProvider(m))
+                {
+                    task.Wait();
+                }
+                else
+                {
+                    tasks.Add(task);
+                }
             }
 
             Task.WaitAll(tasks.ToArray());
         }
 
-        private async Task ProcessEntryAsync(IYamlObject entry, IExecutionContext context)
+        private async Task ProcessEntryAsync(YamlMappingObject entry, IExecutionContext context)
         {
-            context.ExpressionEvaluator.EvaluateExpressions(entry);
-            if (entry is YamlMappingObject m)
-            {
-                var providerName = m.Children.First().Key;
-                var propertyName = m.Children.First().Value.Map<string>();
-                var result = await ExecuteProviderAsync(context, entry, providerName);
+            var providerName = entry.Children.First().Key;
+            var propertyName = entry.Children.First().Value.Map<string>();
+            var result = await ExecuteProviderAsync(context, entry, providerName);
 
-                context.Providers[propertyName] = result;
-                context.VerboseWriteAdding(propertyName, result);
-                return;
-            }
-
-            throw new UnexpectedTypeException("Yaml node should be a mapping", entry.Line, entry.Column);
+            context.Providers[propertyName] = result;
+            context.VerboseWriteAdding(propertyName, result);
         }
 
         private static Task<IYamlObject> ExecuteProviderAsync(IExecutionContext context, IYamlObject entry, string providerName)
@@ -58,5 +63,7 @@ namespace Cac.Interpretation.NodeProcessors
             context.Out.Verbose.WriteLine("`");
             return context.AvailableProviders[key].GetValueAsync(entry, context.YamlConverter, context);
         }
+
+        private static bool IsSyncProvider(YamlMappingObject m) => m.Children.ContainsKey("sync") && m.MapProperty<bool>("sync");
     }
 }
